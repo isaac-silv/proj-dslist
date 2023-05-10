23c0c01bd4b5d6e80e3b9f857d67c874caa814c9
Isaac Silva <isaacaquino.dv@gmail.com>
2023-05-09 21:34:14 -0300

    Adicionando o endpont getGameById

--- backend/dslist/src/main/java/com/javaspring/dslist/controllers/GameController.java
+++ backend/dslist/src/main/java/com/javaspring/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.javaspring.dslist.dto.GameDTO;
 import com.javaspring.dslist.dto.GameMinDTO;
 import com.javaspring.dslist.service.GameService;
 
@@ -17,10 +19,14 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
+    }
+
     @GetMapping
     public List<GameMinDTO> findAll() {
-        List<GameMinDTO> result = gameService.findAll();
-        return result;
+        return gameService.findAll();
     }
 
 }
