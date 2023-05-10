e54bd9bccd911d6389e6e5a22b86b880f19bc0a9
Isaac Silva <isaacaquino.dv@gmail.com>
2023-05-10 19:37:16 -0300

    Adicionando endpoint seachGameByList

--- backend/dslist/src/main/java/com/javaspring/dslist/service/GameService.java
+++ backend/dslist/src/main/java/com/javaspring/dslist/service/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.javaspring.dslist.dto.GameDTO;
 import com.javaspring.dslist.dto.GameMinDTO;
 import com.javaspring.dslist.entities.Game;
+import com.javaspring.dslist.projections.GameMinProjection;
 import com.javaspring.dslist.repository.GameRepository;
 
 @Service
@@ -28,4 +29,11 @@ public class GameService {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
 }
